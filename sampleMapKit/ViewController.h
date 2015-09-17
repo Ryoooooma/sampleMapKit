a6a1f02efe05744155497bf9c4231c73086a8a80
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-09-17 10:09:16 +0800

    Initial Commit

---
+++ sampleMapKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleMapKit
+//
+//  Created by 脇田竜馬 on 2015/09/17.
+//  Copyright (c) 2015年 Ryoma Wakita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
